6b8b699544bd7adc0bae2c5f50d6d39dc1d497d0
Choo <choo000407@naver.com>
2023-09-01 06:28:52 +0900

    refactor: 메서드 분리해서 depth 감소 (#12)

--- src/main/java/app/lastpang/hour/domain/user/service/UserService.java
+++ src/main/java/app/lastpang/hour/domain/user/service/UserService.java
@@ -5,8 +5,6 @@ import app.lastpang.hour.domain.user.domain.repository.UserRepository;
 import app.lastpang.hour.domain.user.mapper.UserMapper;
 import app.lastpang.hour.domain.user.presentation.dto.request.UserSaveRequest;
 import app.lastpang.hour.domain.user.presentation.dto.response.UserFindOneResponse;
-import app.lastpang.hour.global.exception.CustomException;
-import app.lastpang.hour.global.exception.ErrorCode;
 import app.lastpang.hour.global.utils.UserUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;

c254abc0f70d45e5a9aa3403cdf18ad0e0c789a6
ChooSeoYeon <choo000407@naver.com>
2023-09-01 03:41:47 +0900

    refactor: user에 mapper 적용

--- src/main/java/app/lastpang/hour/domain/user/presentation/dto/response/UserFindOneResponse.java
+++ src/main/java/app/lastpang/hour/domain/user/presentation/dto/response/UserFindOneResponse.java
@@ -1,16 +1,12 @@
 package app.lastpang.hour.domain.user.presentation.dto.response;
 
-import app.lastpang.hour.domain.user.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UserFindOneResponse {
 
     private Long id;
     private String name;
-
-    public UserFindOneResponse(User user) {
-        id = user.getId();
-        name = user.getProfile().getName();
-    }
 }
